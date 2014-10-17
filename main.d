a66ad8d294b95c65b24a35f13b3bc93f57ceff81
Ronny <noelbenzinger@gmail.com>
2014-10-17 07:51:02 -0500

    Progress

--- main.d
+++ main.d
@@ -4,6 +4,8 @@ static import conv = std.conv;
 static import file = std.file;
 static import date = std.datetime;
 static import json = std.json;
+static import io = std.stdio;
+static import path = std.path;
 
 string monthToStr(int month)
 {
@@ -51,27 +53,196 @@ int digits(int n)
 	return count;
 }
 
+struct PostData
+{
+	int id;
+	string title;
+	int year;
+	int month;
+	int day;
+	
+	/**
+	 * Fills in any information that can be assessed without user input (ex: date).
+	 */
+	void fill()
+	{
+		date.Date dateObj = cast(date.Date)date.Clock.currTime();
+		year = dateObj.year;
+		month = dateObj.month;
+		day = dateObj.day;
+	}
+	
+	string toJSON()
+	{
+		auto jsonValue = json.JSONValue();
+		
+		json.JSONValue[string] jsonObj;
+		
+		auto idObj = json.JSONValue();
+		idObj.integer = id;
+		jsonObj["id"] = idObj;
+		
+		auto yearObj = json.JSONValue();
+		yearObj.integer = year;
+		jsonObj["year"] = yearObj;
+		
+		auto monthObj = json.JSONValue();
+		monthObj.integer = month;
+		jsonObj["month"] = monthObj;
+		
+		auto dayObj = json.JSONValue();
+		dayObj.integer = day;
+		jsonObj["day"] = dayObj;
+		
+		auto stringObj = json.JSONValue();
+		stringObj.str = title;
+		jsonObj["title"] = stringObj;
+		
+		jsonValue.object = jsonObj;
+		
+		return conv.text(json.toJSON(&jsonValue, true));
+	}
+}
+
+
+struct Post
+{
+	PostData data;
+	string[] parts;
+}
+
+Post generatePost(string content, PostData data, string[] format)
+{
+	string[] parts;
+	parts ~= format[0];
+	parts ~= conv.text(data.id);
+	parts ~= format[1];
+	parts ~= data.title;
+	parts ~= format[2];
+	parts ~= monthToStr(data.month);
+	parts ~= format[3];
+	parts ~= conv.text(data.day);
+	parts ~= format[4];
+	parts ~= conv.text(data.year);
+	parts ~= format[5];
+	parts ~= content;
+	parts ~= format[6];
+	
+	Post post = {data, parts};
+	
+	return post;
+}
+
+/**
+ * Splits *str* at each occurence of *sep* into an array of strings.
+ */
+string[] split(string str, char sep)
+{
+	string[] results;
+	
+	int cur = 0;
+	for (int i = 0; i <= str.length; i++)
+	{
+		if (i == str.length || str[i] == sep)
+		{
+			if (cur < i)
+			{
+				results ~= str[cur .. i];
+			}
+			cur = i+1;
+		}
+	}
+	
+	return results;
+}
+
 void main()
 {
 	
 	auto header = cast(string)file.read("templates/header.html");
-	auto post = cast(string)file.read("templates/post.html");
+	string[] postFmt = split(cast(string)file.read("templates/post.html"), '@');
 	auto style = cast(string)file.read("templates/style.css");
 	
-	date.Date dateObj = cast(date.Date)date.Clock.currTime();
-	int year = dateObj.year;
-	string month = monthToStr(dateObj.month);
-	int day = dateObj.day;
+	Post[int] posts;
 	
-	int size = header.length + style.length;
+	int maxID;
+	string newPostContent;
+	string newPostJsonPath;
 	
-	auto testJson = json.parseJSON(cast(string)file.read("posts/helloworld.json"));
+	foreach(f; file.dirEntries("posts", "*.txt", file.SpanMode.shallow))
+	{
+		auto postContent = cast(string)file.read(f.name);
+		auto jsonPath = path.setExtension(f.name, ".json");
+		
+		if (!file.exists(jsonPath))
+		{
+			newPostContent = postContent;
+			newPostJsonPath = jsonPath;
+		}
+		else
+		{
+			auto jsonTxt = cast(string)file.read(jsonPath);
+			auto value = json.parseJSON(jsonTxt);
+			auto object = value.object;
+			
+			int id = cast(int)object["id"].integer;
+			int year = cast(int)object["year"].integer;
+			int month = cast(int)object["month"].integer;
+			int day = cast(int)object["day"].integer;
+			string title = object["title"].str;
+			
+			PostData data = {id, title, year, month, day};
+			
+			posts[id] = generatePost(postContent, data, postFmt);
+			
+			maxID = id > maxID ? id : maxID;
+		}
+	}
 	
-	print(conv.text(testJson));
-	print(conv.text(testJson.type));
+	if (newPostContent != null)
+	{
+		maxID += 1;
+		
+		auto title = io.readln();
+		if (title.length > 0)
+		{
+			title = title[0 .. $-1];
+		}
+		
+		PostData data = {maxID, title};
+		data.fill();
+		
+		file.write(newPostJsonPath, data.toJSON());
+		
+		posts[maxID] = generatePost(newPostContent, data, postFmt);
+	}
 	
-	auto object = testJson.object;
-	print(monthToStr(cast(int)object["month"].integer));
 	
-	file.write("test.html", "Hi.");
+	string[] blog;
+	int size;
+	
+	blog ~= header;
+	size += header.length;
+	
+	for (int i = 1; i <= posts.length; i++)
+	{
+		auto post = posts[i];
+		blog = blog ~ post.parts;
+		
+		foreach(string part; post.parts)
+		{
+			size += part.length;
+		}
+	}
+	
+	char[] html = new char[size];
+	
+	int p = 0;
+	for (int i = 0; i < blog.length; i++)
+	{
+		auto str = blog[i];
+		html[p .. p+str.length] = str;
+	}
+	
+	file.write("html/index.html", html);
 }
